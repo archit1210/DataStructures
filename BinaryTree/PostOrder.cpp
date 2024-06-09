8641bd55b02d6bda647b8f8027337df8c3a1777a
archit1210 <architlokhande72@gmail.com>
2024-06-09 20:49:03 +0530

    Traversal techniques added

---
+++ BinaryTree/PostOrder.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->data = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *create(Node *root)
+{
+    int data;
+    cout << "Enter the data for the node : " << endl;
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    root = new Node(data);
+
+    cout << "Enter the data for left of " << root->data << " : " << endl;
+    root->left = create(root->left);
+
+    cout << "Enter the data for right of " << root->data << " : " << endl;
+    root->right = create(root->right);
+
+    return root;
+}
+
+void postOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root;
+
+    root = create(root);
+    postOrder(root);
+
+    return 0;
+}
---
+++ BinaryTree/PreOrder.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->data = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *create(Node *root)
+{
+    int data;
+    cout << "Enter the data for the node : " << endl;
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    root = new Node(data);
+
+    cout << "Enter the data for left of " << root->data << " : " << endl;
+    root->left = create(root->left);
+
+    cout << "Enter the data for right of " << root->data << " : " << endl;
+    root->right = create(root->right);
+
+    return root;
+}
+
+void preOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int main()
+{
+    Node *root;
+
+    root = create(root);
+    preOrder(root);
+
+    return 0;
+}
